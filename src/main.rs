338c147c555c4b9f228d138115cdb0f3dac8b59f
Paul Rogalinski-Pinter <paul@paul.vc>
2021-12-26 02:04:16 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,14 @@ use paho_mqtt::AsyncClient;
 use std::convert::TryFrom;
 use matrix_sdk::{
   Client, SyncSettings, Result,
-  ruma::{UserId, events::{SyncMessageEvent, room::message::MessageEventContent}},
+  ruma::{UserId, events::{SyncMessageEvent, room::message::MessageEventContent, room::message::MessageType::Text}},
+  room::Room,
 };
-use matrix_sdk::room::Room;
-use matrix_sdk::ruma::events::room::message::MessageType;
-use matrix_sdk::ruma::events::room::message::MessageType::Text;
 
 extern crate clap;
 
 #[derive(Parser, Clone)]
-#[clap(version = "1.0-beta",
+#[clap(version = "0.2",
 author = "Paul Rogalinski-Pinter, matrix2mqtt@t00ltime.de",
 about = "forwards messages from matrix to mqtt")]
 struct Opts {
@@ -43,20 +41,22 @@ struct Opts {
 #[tokio::main]
 async fn main() -> Result<()> {
   let opts: Opts = Opts::parse();
+  setup_logger(&opts);
+
   let matrix_user = UserId::try_from(opts.matrix_username.as_str())?;
   let matrix_client = Client::new_from_user_id(matrix_user.clone()).await?;
   let mqtt_client = Box::new(connect_mqtt(opts.mqtt_host.as_str(), opts.mqtt_username.as_str(), opts.mqtt_password.as_str()));
-  setup_logger(&opts);
-  matrix_client.login(matrix_user.localpart(), opts.matrix_password.as_str(), None, None).await?;
 
+  matrix_client.login(matrix_user.localpart(), opts.matrix_password.as_str(), None, None).await?;
 
   matrix_client
     .register_event_handler(
       move |ev: SyncMessageEvent<MessageEventContent>,
             room: Room|
         {
+          debug!("Incoming message {:?} on room {:?}",ev,room);
+
           let client = mqtt_client.clone();
-          // let sanitizer = |x| {!['#', '/'].contains(&x)};
           let sanitizer = |c| !r#"#/+"#.contains(c);
           async move {
             let room_name = match room.canonical_alias() {
@@ -99,10 +99,8 @@ async fn main() -> Result<()> {
     )
     .await;
 
-  // Syncing is important to synchronize the client state with the server.
-  // This method will never return.
+  // enters the sync-loop, does not resolve.
   matrix_client.sync(SyncSettings::default()).await;
-
   Ok(())
 }
 
@@ -121,20 +119,24 @@ fn setup_logger(opts: &Opts) {
 
 fn connect_mqtt(host: &str, username: &str, password: &str) -> AsyncClient {
   info!("connecting to mqtt {}",host);
-  let cli = mqtt::AsyncClient::new(host).unwrap_or_else(|err| {
-    error!("Error creating the client: {:?}", err);
+  let mqtt_client = mqtt::AsyncClient::new(host).unwrap_or_else(|err| {
+    error!("Error creating the mqtt client: {:?}", err);
     process::exit(1);
   });
 
-  let conn_opts = mqtt::ConnectOptionsBuilder::new()
-    .keep_alive_interval(Duration::from_secs(20))
+  let mut builder = mqtt::ConnectOptionsBuilder::new();
+  builder.keep_alive_interval(Duration::from_secs(20))
     .automatic_reconnect(Duration::from_secs(1), Duration::from_secs(60))
-    .clean_session(true)
-    .user_name(username).password(password).finalize();
-  cli.connect(conn_opts).wait_for(Duration::from_secs(10)).unwrap_or_else(|err| {
+    .clean_session(true);
+
+  if !username.is_empty() {
+    builder.user_name(username).password(password);
+  }
+
+  mqtt_client.connect(builder.finalize()).wait_for(Duration::from_secs(10)).unwrap_or_else(|err| {
     error!("could not connect to mqtt: {:?}", err);
     process::exit(1);
   });
 
-  cli
+  mqtt_client
 }
